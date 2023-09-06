f59de1b82d7b145772789b8fd547eda2823344a7
alonlevi <alon.levi3@mail.huji.ac.il>
2023-09-06 12:36:45 +0300

    gb 4 -> gb 10

---
+++ gridScriptGAN.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#PBS -m n
+#PBS -l walltime=12:00:00
+
+#### script that run the python script, the MadGraph generator
+echo "Installing python3>>>>>>"
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+alias setRoot='setupATLAS && lsetup "views LCG_101_ATLAS_20 x86_64-centos7-gcc11-opt"'
+setRoot
+
+echo "running the python script >>>>>>"
+n_epoch=${parname1}
+Directory=${parname2}
+
+#### go to the directory where the files live
+cd ${Directory}
+echo "I am now in "${PWD}
+
+time python LUXE_WGAN.py ${n_epoch}
