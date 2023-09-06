ca49d75e5eb859fad53bdd8a3c88fbcea20f65e4
Alon Levi <alonle@wipp-an01.weizmann.ac.il>
2023-09-06 14:11:22 +0300

    WIS changes

--- sendGANJobs.sh
+++ sendGANJobs.sh
@@ -2,11 +2,8 @@
 
 ### how many jobs you want to submit, if -1, then submits jobs you set
 nepoch=${1}
-<<<<<<< HEAD
 runid=0
-=======
 
->>>>>>> f59de1b82d7b145772789b8fd547eda2823344a7
 ### a counter value
 b=1
 ### runId increased by one
@@ -14,31 +11,18 @@ runid=$(( $runid + $b ))
 echo "runid: "$runid
 ### the place where the output and error file of the grid will live
 DESTINATION="/storage/agrp/alonle/ClusterLogs"
-<<<<<<< HEAD
-=======
 ### create the main directory if it does not exists
-mkdir -p ${DESTINATION}/run_${runid}
->>>>>>> f59de1b82d7b145772789b8fd547eda2823344a7
     
 ### if main directory/run_id exists, delete
-#if [[ -d "${DESTINATION}/run_$runid" ]]; then
-#    echo "Found a directory with output ${DESTINATION}/run_$runid! Deleting the previous one."
-#    rm -rf ${DESTINATION}/run_$runid
-#fi
+if [[ -d "${DESTINATION}/run_$runid" ]]; then
+    echo "Found a directory with output ${DESTINATION}/run_$runid! Deleting the previous one."
+    rm -rf ${DESTINATION}/run_$runid
+fi
    
-<<<<<<< HEAD
-mkdir -p ${DESTINATION}/run_${runid}
+mkdir -p ${DESTINATION}/run_${runid}/
 #### from where you are submitting jobs
 PRESENTDIRECTORY=${PWD}
 #### submit jobs to the PBS system
-qsub -l ngpus=1,mem=4gb -v parname1=${nepoch},parname2=${PRESENTDIRECTORY} -q N -N "run_"$runid -o "${DESTINATION}/run_"${runid} -e "${DESTINATION}/run_"${runid} gridScriptGAN.sh
-=======
-#### from where you are submitting jobs
-PRESENTDIRECTORY=${PWD}
-#### submit jobs to the PBS system
-qsub -l ngpus=1,mem=10gb -v parname1=${nepoch},parname2=${PRESENTDIRECTORY} -q N -N "run_"$runid -o "${DESTINATION}/run_"${runid} -e "${DESTINATION}/run_"${runid} gridScriptGAN.sh
->>>>>>> f59de1b82d7b145772789b8fd547eda2823344a7
-### sleep for 1 s, so that there is no problem in submitting jobs to the grid
 sleep 1s
     
 
